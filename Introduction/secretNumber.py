fdd4ab5fe48a7743aa8f056056cc0b0bda1c562c
Adewale OBADIMU <admodams@yahoo.com>
2017-06-09 11:24:16 +0100

    Update secretNumber.py

--- Introduction/secretNumber.py
+++ Introduction/secretNumber.py
@@ -1,14 +1,17 @@
 def SecretNumber():
-    One = int(input("Type a number between  1 and 10: "))
-    Two = int(input("Type a number between 1 and 10:"))
-    
-    if(One >= 1) and (One <= 10):
+    GotIt = False
+    while GotIt == False:
+        One = int(input("Type a number between 1 and 10: "))
+        Two = int(input("Type a number between 1 and 10: "))
         
-        if(Two >= 1) and (Two <=  10):
-            print('Your secret number is: '+ str(One * Two)) 
+        if(One >= 1) and (One <=10):
+            if(Two >= 1) and (Two <= 10):
+                print('Secret number number is : '+ str(One * Two))
+                GotIt = True
+                continue
+            else:
+                print('Incorrect second value!')
         else:
-            print("Incorrect second value!")
-    else:
-        print("Incorrect first value")
-        
+            print('Incorrect first value')
+        print('Try again')
 SecretNumber()
