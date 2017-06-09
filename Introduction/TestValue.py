429750d92eac3568267c6629812887400c465277
Adewale OBADIMU <admodams@yahoo.com>
2017-06-09 11:22:42 +0100

    Create TestValue.py

---
+++ Introduction/TestValue.py
@@ -0,0 +1,9 @@
+def TestValue(Value):
+    if Value == 5:
+        print('Value is 5!')
+    elif Value == 6:
+        print('Value is 6!')
+    else:
+        print('Value is something else.')
+        print('It equals ' + str(Value))
+TestValue(6)
