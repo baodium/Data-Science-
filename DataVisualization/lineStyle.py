7e8a09ce5db791898c8255c6d1952a5ca48da3d9
Adewale OBADIMU <admodams@yahoo.com>
2017-06-15 06:46:29 +0100

    Create lineStyle.py

---
+++ DataVisualization/lineStyle.py
@@ -0,0 +1,7 @@
+values = [1,5,8,9,2,0,3,10,4,7]
+values2 = [3,8,9,2,1,2,4,7,6,6]
+
+import matplotlib.pyplot as plt
+plt.plot(range(1,11), values, ':')
+plt.plot(range(1,11), values, '-.')
+plt.show()
