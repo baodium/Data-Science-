8f14c64e943bafdb2a7665a9620da141e852aad1
Adewale OBADIMU <admodams@yahoo.com>
2017-06-15 06:50:22 +0100

    Create colorPlot.py

---
+++ DataVisualization/colorPlot.py
@@ -0,0 +1,9 @@
+values = [1,5,8,9,2,0,3,10,4,7]
+values2 = [3,8,9,2,1,2,4,7,6,6]
+
+import matplotlib.pyplot as plt
+
+plt.plot(range(1,11), values, 'r')
+plt.plot(range(1,11), values2, 'b')
+
+plt.show()
