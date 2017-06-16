5411ceb06507c436104f22315a3ae25692579307
Adewale OBADIMU <admodams@yahoo.com>
2017-06-16 06:11:58 +0100

    Create histogram.py

---
+++ Visualizing the invisible/histogram.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+x = 20 * np.random.rand(10000)
+
+plt.hist(x, 25, range = (-50, 50), histtype='stepfilled', align='mid', color='g', label='Test Data' )
+plt.legend()
+plt.title('Step Filled Histogram')
+plt.show()
